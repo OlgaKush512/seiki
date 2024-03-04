9c5d2d81dafe2321cdaebd37e0d322ed9ec989f1
Olga <olikafg512@gmail.com>
2024-03-04 21:20:27 +0100

    map

--- src/app/ui/Home.tsx
+++ src/app/ui/Home.tsx
@@ -5,7 +5,7 @@ import { analyse, analyseFilter } from "../lib/actions";
 import dynamic from "next/dynamic";
 import { Filters, seikiData } from "../lib/definitions";
 import SidePanel from "./SidePanel";
-import { Stack, Tab, Tabs } from "@mui/material";
+import { Box, Stack, Tab, Tabs } from "@mui/material";
 import TabContent from "./TabContent";
 
 const DinamicMap = dynamic(() => import("./DinamicMap"), { ssr: false });
@@ -46,97 +46,104 @@ const Home = () => {
   console.log("formData?.kpi", formData?.kpi);
   return (
     <div>
-      <Stack direction="row" sx={{ border: "solid" }}>
-        <SidePanel data={data} onSubmit={handleSubmit} />
-        {filtredData !== null && (
-          <div>
-            <Stack direction="row">
-              <div>
-                <Tabs
-                  value={selectedTab}
-                  onChange={handleTabChange}
-                  aria-label="Charts tabs"
-                >
-                  <Tab value="ages" label="AGE" />
-                  <Tab value="gender" label="GENDER" />
-                  <Tab value="hour" label="HOUR" />
-                  <Tab value="day" label="DAY" />
-                  <Tab value="mode" label="MODE" />
-                  <Tab value="purpose" label="TRIP PURPOSE" />
-                  <Tab value="week" label="WEEK " />
-                </Tabs>
-                {selectedTab === "ages" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="age_breakdown"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (ages)"
-                    labelPeople="Traffic: People in a day (ages)"
-                  />
-                )}
-                {selectedTab === "gender" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="gender_breakdown"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (gender)"
-                    labelPeople="Traffic: People in a day (gender)"
-                  />
-                )}
-                {selectedTab === "hour" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="hour_breakdown"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (hour)"
-                    labelPeople="Traffic: People in a day (hour)"
-                  />
-                )}
-                {selectedTab === "day" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="day_type_ratio"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (day type)"
-                    labelPeople="Traffic: People in a day (day type)"
-                  />
-                )}
-                {selectedTab === "mode" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="mode_breakdown"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (mode)"
-                    labelPeople="Traffic: People in a day (mode)"
-                  />
-                )}
-                {selectedTab === "purpose" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="trip_purpose_breakdown"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (trip purpose)"
-                    labelPeople="Traffic: People in a day (trip purpose)"
-                  />
-                )}
-                {selectedTab === "week" && (
-                  <TabContent
-                    filtredData={filtredData}
-                    nameOfData="week_ratio"
-                    formData={formData}
-                    labelCrossing="Traffic: Crossings in a day (week)"
-                    labelPeople="Traffic: People in a day (week)"
-                  />
-                )}
-              </div>
+      {/* <Stack direction="row" sx={{ border: "solid" }}> */}
+      <SidePanel data={data} onSubmit={handleSubmit} />
+      {filtredData !== null && (
+        <div>
+          <Stack direction="row">
+            <div>
+              <Tabs
+                value={selectedTab}
+                onChange={handleTabChange}
+                aria-label="Charts tabs"
+              >
+                <Tab value="ages" label="AGE" />
+                <Tab value="gender" label="GENDER" />
+                <Tab value="hour" label="HOUR" />
+                <Tab value="day" label="DAY" />
+                <Tab value="mode" label="MODE" />
+                <Tab value="purpose" label="TRIP PURPOSE" />
+                <Tab value="week" label="WEEK " />
+              </Tabs>
+              {selectedTab === "ages" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="age_breakdown"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (ages)"
+                  labelPeople="Traffic: People in a day (ages)"
+                />
+              )}
+              {selectedTab === "gender" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="gender_breakdown"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (gender)"
+                  labelPeople="Traffic: People in a day (gender)"
+                />
+              )}
+              {selectedTab === "hour" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="hour_breakdown"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (hour)"
+                  labelPeople="Traffic: People in a day (hour)"
+                />
+              )}
+              {selectedTab === "day" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="day_type_ratio"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (day type)"
+                  labelPeople="Traffic: People in a day (day type)"
+                />
+              )}
+              {selectedTab === "mode" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="mode_breakdown"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (mode)"
+                  labelPeople="Traffic: People in a day (mode)"
+                />
+              )}
+              {selectedTab === "purpose" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="trip_purpose_breakdown"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (trip purpose)"
+                  labelPeople="Traffic: People in a day (trip purpose)"
+                />
+              )}
+              {selectedTab === "week" && (
+                <TabContent
+                  filtredData={filtredData}
+                  nameOfData="week_ratio"
+                  formData={formData}
+                  labelCrossing="Traffic: Crossings in a day (week)"
+                  labelPeople="Traffic: People in a day (week)"
+                />
+              )}
+            </div>
+            <Box
+              sx={{ border: "solid", width: "100%" }}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
               <DinamicMap
                 key={filtredData.items[0].id}
                 selectedAddress={filtredData.items[0]}
               />
-            </Stack>
-          </div>
-        )}
-      </Stack>
+            </Box>
+          </Stack>
+        </div>
+      )}
+      {/* </Stack> */}
     </div>
   );
 };
