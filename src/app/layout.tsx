bead3c567666f162aa938db0bee6b271ab190ad4
Olga <Olga@Brunos-MacBook-Air.local>
2024-03-05 18:23:05 +0100

    clean code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
